b0c3ecaee7757204bc63679c509872f71059027b
Fernando Rocha <beneditofernandor@gmail.com>
2025-06-19 17:12:05 -0300

    feat: contato controller

---
+++ src/main/java/com/fernandorocha/nubank/controller/ContatoController.java
@@ -0,0 +1,46 @@
+package com.fernandorocha.nubank.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fernandorocha.nubank.dto.ContatoDTO;
+import com.fernandorocha.nubank.model.Clientes;
+import com.fernandorocha.nubank.model.Contato;
+import com.fernandorocha.nubank.repository.ClientesRepository;
+import com.fernandorocha.nubank.repository.ContatoRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/contatos")
+public class ContatoController {
+
+    @Autowired
+    private ContatoRepository contatoRepository;
+
+    @Autowired
+    private ClientesRepository clientesRepository;
+
+    @PostMapping
+    public ResponseEntity<?> criar(@RequestBody ContatoDTO dto) {
+
+        Optional<Clientes> clientesOpt = clientesRepository.findById(dto.getClienteId());
+
+        if (clientesOpt.isEmpty()) {
+            return ResponseEntity.badRequest().body("Cliente não encontrado");
+        }
+        Contato contato = new Contato();
+        contato.setTelefone(dto.getTelefone());
+        contato.setEmail(dto.getEmail());
+        contato.setClientes(clientesOpt.get());
+        contatoRepository.save(contato);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(contato);
+    }
+
+}
